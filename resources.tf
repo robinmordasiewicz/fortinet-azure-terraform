5a9cd88a10aa790551abf28e121e6e72e21da12e
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-05-31 00:45:50 -0400

    update README

--- resources.tf
+++ resources.tf
@@ -94,7 +94,7 @@ resource "random_id" "storage_account" {
     resource_group = azurerm_resource_group.rg[count.index].name
   }
   prefix      = var.resource_group_name_prefix
-  byte_length = 1
+  byte_length = 2
 }
 
 # Create storage account for boot diagnostics
